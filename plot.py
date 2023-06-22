b7e244658c41af10a3324b318e2fb9ed671e8352
Jeffrey LEE <jeffreyleeon@gmail.com>
2023-06-21 22:27:01 -0700

    first commit

---
+++ plot.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+data_json = {}
+
+with open('raw.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\tIn {row["Year"]} there are {row["Live Births"]} {row["Race"]} were born.')
+        race = row["Race"].split(" ")[0]
+        births = int(row["Live Births"])
+        if race == 'All':
+            continue
+        if data_json.get(row["Race"]) is None:
+            data_json[race] = births
+        else:
+            data_json[race] += births
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+print(data_json.keys())
+print(data_json.values())
+
+x = np.array(list(data_json.keys()))
+y = np.array(list(data_json.values()))
+
+plt.bar(x,y)
+plt.show()
